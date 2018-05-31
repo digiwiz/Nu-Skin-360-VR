0eaa7601689f073bae028d0d93ff8cc1ac3b0e00
digiwiz <dterenzio@gmail.com>
2018-05-31 09:11:33 -0400

    Initial Checkin
    Initial Checkin

---
+++ Nu Skin 360 VR/Assets/Hotspot.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+//using DG.Tweening;
+
+public class Hotspot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public GameObject ThisPanorama;
+    public GameObject TargetPanorama;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0.5f, 0);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnPointerExit(eventData);
+        OnHotspotTransition();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale += new Vector3(0.08f, 0.08f, 0.08f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
+    }
+
+    public void OnHotspotTransition()
+    {
+        SetSkyBox();
+    }
+
+    private void SetSkyBox()
+    {
+        if (TourManager.SetCameraPosition != null)
+            TourManager.SetCameraPosition(TargetPanorama.transform.position, ThisPanorama.transform.position);
+        TargetPanorama.gameObject.SetActive(true);
+        ThisPanorama.gameObject.SetActive(false);
+    }
+}
